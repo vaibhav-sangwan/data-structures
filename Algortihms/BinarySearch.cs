36e1a89567208aa75511db7b89416ba407eebba6
Vaibhav Sangwan <sangwanvaibhav02@gmail.com>
2022-05-25 21:53:11 +0530

    Implemented Binary Search algorithm on a sorted array

---
+++ Algortihms/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures.Algortihms
+{
+    class BinarySearch
+    {
+        private static int binarySearch(int[] arr, int start, int end, int target)
+        {
+            if(start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                Console.WriteLine(arr[mid]);
+                if (arr[mid] == target)
+                    return mid;
+                if (arr[mid] > target)
+                    return binarySearch(arr, start, mid - 1, target);
+                else
+                    return binarySearch(arr, mid + 1, end, target);
+            }
+            return -1;
+        }
+
+        public int findIndex(int[] arr, int target)
+        {
+            return binarySearch(arr, 0, arr.Length - 1, target);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,30 +2,17 @@
 using System.Collections.Generic;
 using System.Collections;
 
-namespace Data_Structures
+namespace Data_Structures.Algortihms
 {
 
     class Program
     {
         static void Main(string[] args)
         {
-            BinarySearchTree BST = new BinarySearchTree();
-            BST.Insert(11);
-            BST.Insert(8);
-            BST.Insert(3);
-            BST.Insert(1);
-            BST.Insert(6);
-            BST.Insert(12);
-            BST.Insert(17);
-            BST.Insert(13);
-            BST.Insert(19);
-            BST.Insert(14);
-            BST.Insert(5);
-            BST.Insert(15);
-            BST.LevelOrderTraversal();
-            BST.Remove(17);
-            Console.WriteLine();
-            BST.LevelOrderTraversal();
+            int[] arr = { 1, 13, 24, 33, 54, 101, 123, 145, 233, 473, 2341 };
+            BinarySearch bs = new BinarySearch();
+            Console.WriteLine(bs.findIndex(arr, 473));
+
 
 
         }
